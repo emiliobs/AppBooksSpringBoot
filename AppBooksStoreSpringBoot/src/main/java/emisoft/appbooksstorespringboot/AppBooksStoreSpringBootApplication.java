c082df86cd98f2bc8485c1e4d348731fca9f6ef0
Emilio Barrera Sepúlveda <barrera_emilio@yahoo.es>
2023-09-13 22:40:52 -0500

    the index done

---
+++ AppBooksStoreSpringBoot/src/main/java/emisoft/appbooksstorespringboot/AppBooksStoreSpringBootApplication.java
@@ -0,0 +1,15 @@
+package emisoft.appbooksstorespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppBooksStoreSpringBootApplication
+{
+    public static void
+    main(String[] args)
+    {
+        SpringApplication.run(AppBooksStoreSpringBootApplication.class, args);
+    }
+
+}
---
+++ AppBooksStoreSpringBoot/src/test/java/emisoft/appbooksstorespringboot/AppBooksStoreSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package emisoft.appbooksstorespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest class AppBooksStoreSpringBootApplicationTests
+{
+
+    @Test void contextLoads()
+    {
+    }
+
+}
